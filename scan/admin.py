91722c4781ff1137bdd0c4b9f2c7cd320e52dbd3
Robin Hedlund <robin.hedlund@outlook.com>
2015-12-07 11:48:55 +0100

    Fixes to admin.py spotted by DiskJocke

--- scan/admin.py
+++ scan/admin.py
@@ -1,11 +1,10 @@
+# Import defined models from scan/models.py
+
 from django.contrib import admin
+from .models import Scan, Network, Host, Service, Site, Siteuser
 
 # Registration of models are done here, registered models appear in django administration
 
-# Import defined models from scan/models.py
-
-# from .forms import SiteForm
-from .models import Scan, Network, Host, Service, Site, Siteuser
 admin.site.register(Scan)
 admin.site.register(Network)
 admin.site.register(Host)
