6059d807a558c41fcdfb49537555f512092baa8a
Robin Hedlund <robin.hedlund@outlook.com>
2015-12-08 11:25:13 +0100

    Commited views again...

--- scan/views.py
+++ scan/views.py
@@ -9,42 +9,52 @@ from celery.result import AsyncResult
 from .models import Scan, Site, Siteuser, Network, Host, Service
 from .tasks import scanNetwork
 
+# This is the main connection between the back-end and front-end
+# The different statements are further described in the text
+
+# Authentication is used for this view
 @login_required
 def index(request):
 
     network_form = None
-
+    # Use the current site to load information from
     try:
         site_user = Siteuser.objects.get(user=request.user)
         current_site = site_user.current_site
+    # If no site have been created (new user), load "default site"
     except Siteuser.DoesNotExist:
         try:
             default_site = Site.objects.get(default=True)
+        # If "default site" does not exist, create it
         except Site.DoesNotExist:
             default_site = Site(name="Default Site", default=True)
             default_site.save()
         site_user = Siteuser(user=request.user, current_site=default_site)
         site_user.save()
         current_site = default_site
-
+    # Then list the latest scan for the site being used
     try:
         latest_scan = Scan.objects.latest('id')
         if latest_scan:
             task_status = AsyncResult(latest_scan.taskID).ready()
+    # If no scan exist for the site try to list site user and site name instead.
+    # If these are not present either, list nothing
     except Scan.DoesNotExist:
         task_status = True
     try:
         selected_site = request.user.siteuser.current_site.name
     except Siteuser.DoesNotExist:
         selected_site = None
-
+    # Now list the 10 latest networks that were previously scanned
+    # If there are no networks available, list nothing
     try:
         networks = Network.objects.all().order_by('-id').filter(site=current_site)[:10]
     except Network.DoesNotExist:
         networks = None
 
     networks_list = []
-
+    # If there are networks available previously, list the number of host discovered for that network
+    # If there are no hosts, list nothing
     if networks:
         for network in networks:
             network_list = []
@@ -57,14 +67,15 @@ def index(request):
             network_list.append(network)
             network_list.append(len(network_num_hosts))
             networks_list.append(network_list)
-
+    # If hosts have been discovered, list the most recent 10
+    # If no hosts exist, list nothing
     try:
         hosts = Host.objects.all().order_by('-id').filter(site=current_site)[:10]
     except Siteuser.DoesNotExist:
         hosts = None
 
     hosts_list = []
-
+    # If there were hosts available, list the number of services found for each host
     if hosts:
         for host in hosts:
             host_list = []
@@ -77,14 +88,14 @@ def index(request):
             host_list.append(host)
             host_list.append(len(host_num_services))
             hosts_list.append(host_list)
-
+    # List the latest 10 scans if there are any
     try:
         scans = Scan.objects.all().order_by('-id').filter(site=current_site)[:10]
     except Scan.DoesNotExist:
         scans = None
 
     scans_list = []
-
+    # In the list of scans, add a object for each nework that were scanned
     if scans:
         for scan in scans:
             scan_list = []
@@ -104,7 +115,7 @@ def index(request):
         sites = Site.objects.all().order_by('id')
     except Site.DoesNotExist:
         sites = None
-
+    # Objects for this page, this makes it look nice when you zoooom (vector graphics)
     context = RequestContext(request, {
         'selected_site': selected_site,
         'network_form': network_form,
@@ -114,14 +125,17 @@ def index(request):
         'sites': sites,
         'task_status': task_status,
     })
-
+    # The result of this will be found in the overview tab of the web application
     return render(request, 'scan/overview.html', context)
 
+# Authentication is used for this view
 @login_required
 def scan(request):
+    # Use the current site to load information from
     try:
         site_user = Siteuser.objects.get(user=request.user)
         current_site = site_user.current_site
+    # If no site have been created (new user), load "default site"
     except Siteuser.DoesNotExist:
         try:
             default_site = Site.objects.get(default=True)
@@ -131,26 +145,26 @@ def scan(request):
         site_user = Siteuser(user=request.user, current_site=default_site)
         site_user.save()
         current_site = default_site
-
+    # Load the latest scans if there are any available
     try:
         latest_scan = Scan.objects.latest('id')
         task_status = latest_scan.ready
     except Scan.DoesNotExist:
         task_status = True
-
+    # Load this for the current site
     try:
         sites = Site.objects.all().order_by('id')
     except Site.DoesNotExist:
         sites = None
 
-
+    # List the 20 latest created networks
     try:
         networks = Network.objects.all().order_by('-id').filter(site=current_site)[:20]
     except Network.DoesNotExist:
         networks = None
 
     networks_list = []
-
+    # List the number of hosts found for these networks if they have been scanned
     if networks:
         for network in networks:
             network_list = []
@@ -163,14 +177,14 @@ def scan(request):
             network_list.append(network)
             network_list.append(len(network_num_hosts))
             networks_list.append(network_list)
-
+    # List the 10 latest network scans
     try:
         scans = Scan.objects.all().order_by('-id').filter(site=current_site)[:10]
     except Scan.DoesNotExist:
         scans = None
 
     scans_list = []
-
+    # For the list of scans, create a object for the network that were scanned
     if scans:
         for scan in scans:
             scan_list = []
@@ -185,14 +199,14 @@ def scan(request):
             scan_list.append(scan)
             scan_list.append(networks)
             scans_list.append(scan_list)
-
+    # Objects used for this page
     context = RequestContext(request, {
         'sites': sites,
         'networks_list': networks_list,
         'scans_list': scans_list,
         'task_status': task_status,
     })
-
+    # This will be applied in the scan tab of the web application
     return render(request, 'scan/scan.html', context)
 
 @login_required
